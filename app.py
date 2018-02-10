3eb595f5a53bec53bda940052ea8aa3d9a13feaf
Justin Guyton <guyton.justin@wustl.edu>
2018-02-09 22:14:16 -0600

    env setup

---
+++ app.py
@@ -0,0 +1,16 @@
+import os,sys
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__) 
+app.debug = True
+
+# PostgreSQL for Server
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__': 
+	app.run()
