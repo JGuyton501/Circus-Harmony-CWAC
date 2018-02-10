df66a9aec2630ad50b99d509f7e15a771d3848e7
Tony De La Nuez <tony.delanuez@gmail.com>
2018-02-10 11:39:48 -0600

    Adding routes.

--- app.py
+++ app.py
@@ -1,16 +1,56 @@
 import os,sys
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+import requests
+import json
 
-app = Flask(__name__) 
+app = Flask(__name__)
 app.debug = True
-
+db = SQLAlchemy()
+db.init_app(app)
 # PostgreSQL for Server
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
+POSTGRES = {
+    'user': 'postgres',
+    'pw' : 'password',
+    'db' : 'circus',
+    'host': 'localhost',
+    'port': '5432',
+}
+employees_url = "http://www.json-generator.com/api/json/get/cfKzddmdlu?indent=2"
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
 @app.route('/')
-def home():
+def main():
     return render_template('home.html')
 
-if __name__ == '__main__': 
-	app.run()
+@app.route('/employees')
+def getEmployees():
+    response = requests.get(employees_url)
+    if (response.ok):
+        json_data = json.loads(response.content)
+        return json.dumps(json_data)
+    else:
+        return 'Could not decode JSON response'
+
+@app.route('/employees/<employee_id>')
+def getEmployee(employee_id):
+    req_id = int(employee_id)
+    response = requests.get(employees_url)
+    if (response.ok):
+        json_data = json.loads(response.content)
+        for emp in json_data:
+            if emp['id'] == req_id:
+                return json.dumps(emp)
+    return 'could not find employee'
+
+@app.route('/dashboard')
+def getDashboard():
+    return 'Not yet.'
+
+if __name__ == '__main__':
+    app.run()
