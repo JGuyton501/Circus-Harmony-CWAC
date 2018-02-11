bd814186a5b023f65d3da5a73cad01839aa418f2
Jeremy <jeremy.p.reed@gmail.com>
2018-02-10 21:26:45 -0600

    updated charts, updated template dir structure

--- static/js/category-controller.js
+++ static/js/category-controller.js
@@ -8,7 +8,7 @@ app.config(function($interpolateProvider){
 	$scope.categories = null;
 
 
-		$scope.getCategories = function(){
+	$scope.getCategories = function(){
 
 		var config = {};
 		config.method = 'get';
@@ -31,7 +31,91 @@ app.config(function($interpolateProvider){
 
 	};
 
+	$scope.renderChart = function(){
+
+
+        var categoryCtx = document.getElementById('categories-chart').getContext('2d');
+        console.log(categoryCtx);
+
+
+var barChartData = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [{
+                label: 'Dataset 1',
+                backgroundColor: 'red',
+                data: [
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor()
+                ]
+            }, {
+                label: 'Dataset 2',
+                backgroundColor: 'blue',
+                data: [
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor()
+                ]
+            }, {
+                label: 'Dataset 3',
+                backgroundColor: 'green',
+                data: [
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor(),
+                    $scope.randomScalingFactor()
+                ]
+            }]
+
+        };
+
+        var config = {
+            type: 'bar',
+            data: barChartData,
+            options: {
+                title:{
+                    display:true,
+                    text:"Chart.js Bar Chart - Stacked"
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false
+                },
+                responsive: true,
+                scales: {
+                    xAxes: [{
+                        stacked: true,
+                    }],
+                    yAxes: [{
+                        stacked: true
+                    }]
+                }
+            }
+        };
+
+        $scope.categoriesChart = new Chart(categoryCtx, config);
+
+        console.log($scope.categoriesChart);
+
+	};
+
+    $scope.randomScalingFactor = function() {
+        return Math.round(Math.random() * 100);
+    };
 
 	$scope.getCategories();
+
+	$scope.renderChart();
 
 });
--- static/js/notification-controller.js
+++ static/js/notification-controller.js
@@ -3,11 +3,11 @@ var app = app || angular.module('circusApp', []);
 app.config(function($interpolateProvider){
 	$interpolateProvider.startSymbol('{[').endSymbol(']}');
 })
-.controller('DashboardController', function($scope, $http) {
+.controller('NotificationController', function($scope, $http) {
 
 	$scope.init = function(){
 		$scope.notifications = [];
-		$scope.notifications.push({},{},{},{},{},{},{},{},{})
+		$scope.notifications.push({},{},{},{},{},{},{},{},{},{},{},{});
 	}
 
 
