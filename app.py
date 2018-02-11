76a33e4c041f9e52a90a876e33b7501c48cd5ec9
Justin Guyton <guyton.justin@wustl.edu>
2018-02-11 00:44:03 -0600

    updated category route

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ import os,sys
 import datetime
 from flask import Flask, render_template, request, url_for, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from dateutil import parser
 import requests
 import json
 import psycopg2
+import pytz
+
 
 app = Flask(__name__)
 app.debug = True
@@ -14,6 +17,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 db.init_app(app)
 import models
+utc=pytz.UTC
 
 
 def dateconverter(o):
@@ -24,7 +28,7 @@ def dateconverter(o):
 def main():
     return render_template('home.html')
 
-@app.route('/employees', methods=['GET']))
+@app.route('/employees', methods=['GET'])
 def getEmployees():
     employees = db.session.query(models.User).all()
     response = []
@@ -286,6 +290,31 @@ def deleteCategory():
         'message': "Category deleted.",
     }
     return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
+
+@app.route('/updateCategory', methods=['POST'])
+def updateCategory():
+	content = request.get_json()
+	o_name = content.get('name')
+	o_job = content.get('job')
+	o_location = content.get('location')
+	o_start_time = parser.parse(content.get('start_time'))
+	o_end_time = parser.parse(content.get('end_time'))
+	new_category_name = content.get('new_category_name')
+
+	categories = db.session.query(models.Category).all()
+	for item in categories:
+		item_dict = item.__dict__
+		print item_dict
+		if item_dict['job'] == o_job and item_dict['location'] == o_location and utc.localize(item_dict['start_time']) >= o_start_time and utc.localize(item_dict['end_time']) <= o_end_time:
+			category = db.session.query(models.Category).get(item_dict['category_id'])
+			category.name = new_category_name
+			db.session.commit()
+
+	response = {
+        'status': 200,
+        'message': "Categories updated.",
+    }
+	return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))	
 
 @app.route('/dashboard')
 def getDashboard():
