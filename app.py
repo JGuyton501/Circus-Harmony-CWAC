9cd5f9b139cb9199fbd881f0e86b966e65af9a65
Tony De La Nuez <tony.delanuez@gmail.com>
2018-02-10 19:02:50 -0600

    Changing models, adding extra routes

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import os,sys
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import requests
 import json
 import psycopg2
-from tools import *
 
 app = Flask(__name__)
 app.debug = True
-
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/circus'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -16,24 +14,9 @@ db = SQLAlchemy(app)
 db.init_app(app)
 import models
 
-
-@app.template_global()
-def static_include(filename):
-    fullpath = os.path.join(app.static_folder, filename)
-    with open(fullpath, 'r') as f:
-        return f.read()
-
 @app.route('/')
 def main():
     return render_template('home.html')
-
-@app.route('/admin/shift')
-def createShift():
-	return render_template('addShift.html')
-
-@app.route('/schedule')
-def schedule():
-    return render_template('schedule.html')
 
 @app.route('/employees')
 def getEmployees():
@@ -49,8 +32,7 @@ def getEmployees():
             })
     return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
 
-
-@app.route('/employees/<employee_id>')
+@app.route('/employees/<employee_id>', methods=['GET'])
 def getEmployee(employee_id):
     employee = db.session.query(models.User).get(employee_id)
     emp_dict = employee.__dict__
@@ -62,26 +44,58 @@ def getEmployee(employee_id):
     }
     return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
 
+@app.route('/addEmployee', methods=['POST'])
+def addEmployee():
+    content = request.get_json()
+    employee = models.User(
+        content.get('first_name'),
+        content.get('last_name'),
+        content.get('phone_number'),
+        content.get('email_address')
+        )
+    db.session.add(employee)
+    db.session.commit()
+    response = {
+        'status': 200,
+        'message': "Employee added to database",
+    }
+    return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
 
+@app.route('/categories', methods=['GET'])
+def getCategories():
+    categories = db.session.query(models.Category).all()
+    response = []
+    for val in categories:
+        cat = val.__dict__
+        response.append({
+            'id': cat['category_id'],
+            'location': cat['location'],
+            'job': cat['job'],
+            'name': cat['name']
+            })
+    return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
 
-@app.route('/employees/shift/<employee_id>')
-def getEmployeeShifts(employee_id):
-	shifts = []
-	employee = db.session.query(models.Shift).filter(models.Shift.employee_id == employee_id).all()
-	for emp in employee:
-		emp_dict = emp.__dict__
-		response = {
-	    	'shift_id': emp_dict['shift_id'],
-			'employee_id': emp_dict['employee_id'],
-			'date': emp_dict['date'],
-			'start_time': emp_dict['start_time'],
-			'end_time': emp_dict['end_time'],
-			'location': emp_dict['location'],
-			'category_id': emp_dict['category_id'],
-			'complete': emp_dict['complete']
-	    }
-		shifts.append(response)	
-	return json.dumps(shifts, sort_keys=True, indent=4, separators=(',', ': '), default = date_convert)
+@app.route('/addShift', methods=['POST'])
+def addShift():
+    content = request.get_json()
+    shift = models.Shift(
+        content.get('employee_id'),
+        content.get('date'),
+        content.get('start_time'),
+        content.get('end_time'),
+        content.get('location'),
+        None,
+        content.get('complete')
+    )
+    db.session.add(shift)
+    db.session.commit()
+    response = {
+        'status': 200,
+        'message': "Employee added to database",
+    }
+    return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
+
+@app.route('/addCategory')
 
 @app.route('/dashboard')
 def getDashboard():
