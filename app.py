fb6626c0aff9863d5a8653554059460e27918340
Tony De La Nuez <tony.delanuez@gmail.com>
2018-02-10 20:09:06 -0600

    More routes

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import os,sys
+import datetime
 from flask import Flask, render_template, request, url_for, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from tools import *
 import requests
 import json
 import psycopg2
-
 
 app = Flask(__name__)
 app.debug = True
@@ -15,6 +14,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 db.init_app(app)
 import models
+
+
+def dateconverter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
 
 @app.route('/')
 def main():
@@ -46,25 +50,6 @@ def getEmployee(employee_id):
     }
     return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
 
-@app.route('/employees/shift/<employee_id>')
-def getEmployeeShifts(employee_id):
-	shifts = []
-	employee = db.session.query(models.Shift).filter(models.Shift.employee_id == employee_id).all()
-	for emp in employee:
-		emp_dict = emp.__dict__
-		response = {
-	    	'shift_id': emp_dict['shift_id'],
-			'employee_id': emp_dict['employee_id'],
-			'date': emp_dict['date'],
-			'start_time': emp_dict['start_time'],
-			'end_time': emp_dict['end_time'],
-			'location': emp_dict['location'],
-			'category_id': emp_dict['category_id'],
-			'complete': emp_dict['complete']
-	    }
-		shifts.append(response)	
-	return json.dumps(shifts, sort_keys=True, indent=4, separators=(',', ': '), default = date_convert)
-
 @app.route('/addEmployee', methods=['POST'])
 def addEmployee():
     content = request.get_json()
@@ -82,6 +67,25 @@ def addEmployee():
     }
     return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
 
+@app.route('/deleteEmployee', methods=["POST"])
+def deleteEmployee():
+    content = request.get_json()
+    delete_id = content.get('employee_id')
+    if not delete_id:
+        response = {
+            'status': 403,
+            'message': "Employee did not exist",
+        }
+        return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
+    employee = db.session.query(models.User).get(delete_id)
+    db.session.delete(employee)
+    db.session.commit()
+    response = {
+        'status': 200,
+        'message': "Employee deleted.",
+    }
+    return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
+
 @app.route('/categories', methods=['GET'])
 def getCategories():
     categories = db.session.query(models.Category).all()
@@ -95,6 +99,25 @@ def getCategories():
             'name': cat['name']
             })
     return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
+
+
+@app.route('/shifts')
+def getShifts():
+    shifts = db.session.query(models.Shift).all()
+    response = []
+    for val in shifts:
+        s = val.__dict__
+        response.append({
+            'shift_id': s['shift_id'],
+            'employee_id': s['employee_id'],
+            'date': s['date'],
+            'start_time': s['start_time'],
+            'end_time': s['end_time'],
+            'location': s['location'],
+            'category_id': s['category_id'],
+            'complete': s['complete']
+            })
+    return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '), default=dateconverter)
 
 @app.route('/addShift', methods=['POST'])
 def addShift():
@@ -112,7 +135,116 @@ def addShift():
     db.session.commit()
     response = {
         'status': 200,
-        'message': "Employee added to database",
+        'message': "Shift added to database",
+    }
+    return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
+
+@app.route('/deleteShift', methods=['POST'])
+def deleteShift():
+    content = request.get_json()
+    delete_id = content.get('shift_id')
+    if not delete_id:
+        response = {
+            'status': 403,
+            'message': "Shift did not exist",
+        }
+        return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
+    shift = db.session.query(models.Shift).get(delete_id)
+    db.session.delete(shift)
+    db.session.commit()
+    response = {
+        'status': 200,
+        'message': "Shift deleted.",
+    }
+    return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
+
+@app.route('/jobs')
+def getJobs():
+    jobs = db.session.query(models.Job).all()
+    response = []
+    for val in jobs:
+        j = val.__dict__
+        response.append({
+            'job_id': j['job_id'],
+            'name': j['name'],
+            })
+    return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
+
+@app.route('/addJob', methods=['POST'])
+def addJob():
+    content = request.get_json()
+    job = models.Job(
+        content.get('name')
+    )
+    db.session.add(job)
+    db.session.commit()
+    response = {
+        'status': 200,
+        'message': "Job added to database",
+    }
+    return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
+
+@app.route('/deleteJob', methods=['POST'])
+def deleteJob():
+    content = request.get_json()
+    delete_id = content.get('job_id')
+    if not delete_id:
+        response = {
+            'status': 403,
+            'message': "Job did not exist",
+        }
+        return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
+    job = db.session.query(models.Job).get(delete_id)
+    db.session.delete(job)
+    db.session.commit()
+    response = {
+        'status': 200,
+        'message': "Job deleted.",
+    }
+    return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
+
+@app.route('/locations')
+def getLocations():
+    locations = db.session.query(models.Location).all()
+    response = []
+    for val in locations:
+        loc = val.__dict__
+        response.append({
+            'location_id': loc['location_id'],
+            'name': loc['name'],
+            })
+    return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
+
+@app.route('/addLocation', methods=['POST'])
+def addLocation():
+    content = request.get_json()
+    location = models.Location(
+        content.get('name')
+    )
+    db.session.add(location)
+    db.session.commit()
+    response = {
+        'status': 200,
+        'message': "Location added to database",
+    }
+    return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
+
+@app.route('/deleteLocation', methods=['POST'])
+def deleteLocation():
+    content = request.get_json()
+    delete_id = content.get('location_id')
+    if not delete_id:
+        response = {
+            'status': 403,
+            'message': "Location did not exist",
+        }
+        return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
+    location = db.session.query(models.Location).get(delete_id)
+    db.session.delete(location)
+    db.session.commit()
+    response = {
+        'status': 200,
+        'message': "Location deleted.",
     }
     return json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))
 
