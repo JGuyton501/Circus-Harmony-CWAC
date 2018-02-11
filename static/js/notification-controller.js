a154de2d5833f5ed98c88dcffab9471095de7a00
Jeremy <jeremy.p.reed@gmail.com>
2018-02-11 09:51:01 -0600

    Added lorem ipsum, styles, quick fixes

--- static/js/notification-controller.js
+++ static/js/notification-controller.js
@@ -7,8 +7,30 @@ app.config(function($interpolateProvider){
 
 	$scope.init = function(){
 		$scope.notifications = [];
-		$scope.notifications.push({},{},{},{},{},{},{},{},{},{},{},{});
+		$scope.notifications.push(
+{"message":"Cras fringilla nunc eget urna sodales, sit amet fermentum arcu varius. Sed libero libero, blandit quis libero eget, vulputate vulputate mi.",
+"header":"ed libero libero"},
+{"message":"Cras fringilla nunc eget urna sodales, sit amet fermentum arcu varius. Sed libero libero, blandit quis libero eget, vulputate vulputate mi.",
+"header":"Nulla eu imperdiet est"},
+{"message":"Etiam congue suscipit nulla ac interdum. Praesent sollicitudin gravida lorem at viverra. ",
+"header":"Praesent sollicitudin"},
+{"message":"Cras fringilla nunc eget urna sodales, sit amet fermentum arcu varius. Sed libero libero, blandit quis libero eget, vulputate vulputate mi.",
+"header":"Nulla eu imperdiet est"},
+{"message":"Etiam congue suscipit nulla ac interdum. Praesent sollicitudin gravida lorem at viverra. ",
+"header":"Praesent sollicitudin"},
+{"message":"Cras fringilla nunc eget urna sodales, sit amet fermentum arcu varius. Sed libero libero, blandit quis libero eget, vulputate vulputate mi.",
+"header":"ed libero libero"},
+{"message":"Cras fringilla nunc eget urna sodales, sit amet fermentum arcu varius. Sed libero libero, blandit quis libero eget, vulputate vulputate mi.",
+"header":"Nulla eu imperdiet est"},
+{"message":"Etiam congue suscipit nulla ac interdum. Praesent sollicitudin gravida lorem at viverra. ",
+"header":"Praesent sollicitudin"},
+{"message":"Cras fringilla nunc eget urna sodales, sit amet fermentum arcu varius. Sed libero libero, blandit quis libero eget, vulputate vulputate mi.",
+"header":"Nulla eu imperdiet est"},
+{"message":"Etiam congue suscipit nulla ac interdum. Praesent sollicitudin gravida lorem at viverra. ",
+"header":"Praesent sollicitudin"}
+			);
 	}
+
 
 
 	$scope.init();
